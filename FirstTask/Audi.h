8d05fe7dc6981592c218b0a139293c5378377944
Mihnea Tanasevici <mihnea.tanasevici@randstaddigital.com>
2024-03-22 12:12:00 +0200

    First task + subtasks

---
+++ FirstTask/Audi.h
@@ -0,0 +1,38 @@
+#include </home/projectx/accademyprojects/FirstTask/Car.h>
+
+/* reguli Singleton
+    1. constructori private
+    2. delete la copy constructor
+    3. static ref catre obiect
+    4. static method pentru a returna ref-ul(pct3)
+*/
+
+
+class Audi : public Car{
+
+    private:
+    Audi(){}
+    //constructor pentru clasa normala care mosteneste Car
+    //Audi(std::string fuel, std::string body, std::string transmission,
+    //     double power, std::string equipment, std::string *vinNumber) : Car(fuel, body, transmission, power, equipment, vinNumber){}
+    ~Audi(){
+        std::cout << "Audi destructor called" << std::endl;
+    }
+    public:
+    std::string fuel;
+    std::string body; 
+    std::string transmission;
+    double power;
+    std::string equipment;
+    std::string *vinNumber;
+    //metoda statica pentru a returna ref
+    static Audi& get_instance(){
+        //static ref catre obiect
+        static Audi instance;
+        return instance;
+    }
+    //delete copy constructor
+    Audi(const Audi&) = delete;
+    void getInfo() override;
+    
+};
--- FirstTask/Car.h
+++ FirstTask/Car.h
@@ -1,11 +1,22 @@
+#pragma once
 #include <iostream>
 
 class Car {
    public:
+      Car(){};
+      Car(std::string fuel, std::string body, std::string transmission,
+         double power, std::string equipment, std::string *vinNumber){
+            this->fuel = fuel;
+            this->body = body;
+            this->transmission = transmission;
+            this->power = power;
+            this-> equipment = equipment;
+            this->vinNumber = vinNumber;
+         }
       // pure virtual function
-      virtual double getInfo() = 0;
-      
-   private:
+      virtual void getInfo() = 0;
+      virtual ~Car(){}
+   protected:
       std::string fuel;
       std::string body;
       std::string transmission;
