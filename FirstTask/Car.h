3ae2b6058f532bd1cbde4a63260a94b6fe495273
Mihnea Tanasevici <mihnea.tanasevici@randstaddigital.com>
2024-03-22 12:35:51 +0200

    Revert "First task + subtasks"
    This reverts commit 8d05fe7dc6981592c218b0a139293c5378377944.

--- FirstTask/Car.h
+++ FirstTask/Car.h
@@ -1,22 +1,11 @@
-#pragma once
 #include <iostream>
 
 class Car {
    public:
-      Car(){};
-      Car(std::string fuel, std::string body, std::string transmission,
-         double power, std::string equipment, std::string *vinNumber){
-            this->fuel = fuel;
-            this->body = body;
-            this->transmission = transmission;
-            this->power = power;
-            this-> equipment = equipment;
-            this->vinNumber = vinNumber;
-         }
       // pure virtual function
-      virtual void getInfo() = 0;
-      virtual ~Car(){}
-   protected:
+      virtual double getInfo() = 0;
+      
+   private:
       std::string fuel;
       std::string body;
       std::string transmission;
