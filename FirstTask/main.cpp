e9a79115bba6fca08f5fe6e1d1aa6e5ed04a5c23
Mihnea Tanasevici <mihnea.tanasevici@randstaddigital.com>
2024-04-09 15:45:51 +0300

    2nd task

--- FirstTask/main.cpp
+++ FirstTask/main.cpp
@@ -2,6 +2,7 @@
 #include </home/projectx/accademyprojects/FirstTask/Opel.h>
 #include </home/projectx/accademyprojects/FirstTask/Audi.h>
 #include </home/projectx/accademyprojects/FirstTask/Rules.cpp>
+#include <memory>
 
 int main(void) {
 
@@ -48,8 +49,39 @@ int main(void) {
     obj6 = std::move(obj5);
 
     std::cout << "obj6: " << *obj6.getVal() << std::endl;
-
+    //apelare fara obj
     Rules::sum();
     
+    //unique ptr
+    //nu poate fi folosit de catre alt pointer(dar poate fi copiat cu move())
+    std::unique_ptr<Opel> newOpel(new Opel());
+    newOpel->getInfo();
+
+    //shared ptr
+    //te lasa sa faci copie la pointer si are use_count() pentru a vedea cati pointeri sunt la adresa respectiva
+    std::shared_ptr<Opel> opel3 = std::make_shared<Opel>();
+    std::shared_ptr<Opel> opel4 = opel3;
+    opel3->getInfo();
+    opel4->getInfo();
+
+    std::cout << "opel3 count: " << opel3.use_count() << std::endl;
+    std::cout << "opel4 count: " << opel4.use_count() << std::endl;
+
+    //weak ptr
+    //este folosit impreuna cu shared ptr, nu are use_count()
+    //si este folosit pentru a iesi dintr-un deadlock, unde pointerii pointeaza intre ei la infinit
+    std::shared_ptr<Opel> opel5 = std::make_shared<Opel>();
+    std::weak_ptr<Opel> opel6;
+    opel6 = opel5;
+    //daca apelam reset lock() nu va mai returna nimic
+    opel5.reset();
+    //lock returneaza un sharedptr din weakptr-ul creat
+    if(auto sharedPtr = opel6.lock()){
+    //putem apela functia daca ptr-ul nu a fost sters
+    sharedPtr->getInfo();
+    }else{
+        std::cout << "ptr-ul a fost sters";
+    }
+
     return 0;
 }
